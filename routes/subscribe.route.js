15a346b077096df874aab12e6129df4c776242e9
kartikeysaxena01 <kartikeysaxenaji4321@gmail.com>
2026-06-09 21:14:49 +0530

    update code

--- routes/subscribe.route.js
+++ routes/subscribe.route.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { subscribeUser, verifyOTP } from "../Controller/Subscriber.controller.js";
-import {otpLimiter} from "../config/ratelimit.js";
+
 
 const router = express.Router();
 
-router.post("/subscribe",otpLimiter,subscribeUser);
-router.post("/verify-otp", otpLimiter, verifyOTP);
+router.post("/subscribe",subscribeUser);
+router.post("/verify-otp", verifyOTP);
 
 export default router;
