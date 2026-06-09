8f5a3f7ebf422f98e10efd019fc93c2ff6f1a602
kartikeysaxena01 <kartikeysaxenaji4321@gmail.com>
2026-06-09 18:50:34 +0530

    updated code

--- config/ratelimit.js
+++ config/ratelimit.js
@@ -1,7 +1,7 @@
 import rateLimit from "express-rate-limit";
 
 export const otpLimiter = rateLimit({
-    windowMs: 5 * 60 * 1000, 
+    windowMs: 6* 60 * 1000, 
     max: 3, 
     message: "Too many OTP requests, please try again later",
     standardHeaders: true,
